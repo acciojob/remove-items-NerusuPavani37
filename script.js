a096c5be5e3ffdc4a677c737492103ac63772f26
Pavani <136723974+NerusuPavani37@users.noreply.github.com>
2025-05-15 20:36:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let select = document.getElementById('colorSelect');
+let btn = document.getElementsByTagName('input')[0];
+
+
+btn.addEventListener('click' ,function(){
+  let selectedIndex = select.selectedIndex;
+  let selectedOption = select.options[selectedIndex];
+  select.removeChild(selectedOption);
+})
